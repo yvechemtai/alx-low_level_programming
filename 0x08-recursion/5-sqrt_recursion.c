dfb61ba660dedc025f70aaa118d90c2a0a8bb8e9
Yvonne <yvechemtai@gmail.com>
2023-03-09 09:51:28 -0800

    0x08-recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,7 +8,7 @@
  * Return: The natural square root of n, or -1 if n does not have a natural
  * square root
  *
- * Author: Silas Mugambi
+ * Author: yvechemtai
  */
 int _sqrt_helper(int n, int i)
 {
@@ -23,5 +23,26 @@ int _sqrt_helper(int n, int i)
 	else
 	{
 		return (_sqrt_helper(n, i + 1));
+	}
+}
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number
+ * @n: The number to find the square root of
+ *
+ * Return: The natural square root of n, or -1 if n does not have a natural
+ * square root
+ *
+ * Author: yvechemtai
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqrt_helper(n, 0));
 	}
 }
