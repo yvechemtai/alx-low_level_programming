4bbb02288886e144ab001f29218d0ee452ee3f8d
yvechemtai <yvechemtai@gmail.com>
2023-03-21 18:54:55 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints out the name of the file being compiled
+ * @void: Null
+ * Return: Success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
